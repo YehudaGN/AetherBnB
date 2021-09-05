5c1be0e84fd918e8819759da58c7c53ece0d124c
yudagn <yudagn@gmail.com>
2021-09-05 11:45:52 -0400

    add staticPages controlelr

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
     validates :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, :fname, :lname, presence: true
     validates :password, length: { minimum: 6, allow_nil: true}
-    validates :bio
+    # validates :bio
 
     attr_reader :password
 
-    after_intialize :ensure_session_token
+    after_initialize :ensure_session_token
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
