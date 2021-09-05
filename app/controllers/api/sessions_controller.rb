dc6804a10360b13270e5597b73a969eeb03d097b
yudagn <yudagn@gmail.com>
2021-09-05 10:40:46 -0400

    add auth branch

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,18 +7,18 @@ class SessionsController < ApplicationController
           )
       
           if @user
-            login(@user)
+            login!(@user)
             render "api/users/show"
           else
-            render json: ["Invalid credentials"], status: 401 # change to correct error?
+            render json: ["Invalid credentials"], status: 401
           end
     end
 
-    def destroy
-        @user = current_user
-        if @user
+    def destroy    
+        if current_user
             logout!
-            render "/api/users/show" # do i render this? 
+            # render "/api/users/show" 
+            render json: {}
         else
             render json: ["Nobody signed in"], status: 404
         end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
 
-    def show
-        @user = User.find_by(id: params[:id])
-        render :show
-    end
+    # def show
+    #     @user = User.find_by(id: params[:id])
+    #     render :show
+    # end
 
     def create
         @user = User.new(user_params)
@@ -12,26 +12,26 @@ class UsersController < ApplicationController
             login!(@user)
             render 'api/users/show' # just sends up to frontend? dont actually need to render the users show in front end i think
         else
-            render json: @user.errors.full_messages
+            render json: @user.errors.full_messages, status: 422
         end
     end
 
-    def update
-        @user = User.find_by(id: params[:id])
-        if @user && @user.update(user_params) # <--- is this wrong
-            render '/api/users/show'
-        else
-            render json: @user.errors.full_messages
-        end
-    end
+    # def update
+    #     @user = User.find_by(id: params[:id])
+    #     if @user && @user.update(user_params) # <--- is this wrong
+    #         render '/api/users/show'
+    #     else
+    #         render json: @user.errors.full_messages
+    #     end
+    # end
 
-    def destroy
+    # def destroy
 
-    end
+    # end
 
     private
 
     def user_params
-        require(:user).permit(:email, :fname, :lname, :password)
+        require(:user).permit(:email, :fname, :lname, :password, :bio)
     end
 end
