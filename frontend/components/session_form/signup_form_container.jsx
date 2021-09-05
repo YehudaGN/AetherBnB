c8bfaf4b3f08f462cb42a3e0d697bd46867308d4
yudagn <yudagn@gmail.com>
2021-09-05 12:03:20 -0400

    create session containers for session modal

---
+++ frontend/components/session_form/signup_form_container.jsx
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import SessionForm from './session_modal';
+import { Link } from 'react-router-dom'; //might need to use this
+import { login } from '../../actions/session_actions'
+
+const mSTP = ({errors}) => ({
+    errors: errors.session
+})
+
+const mDTP = dispatch => ({
+    action: user => dispatch(signup(user))
+})
+
+export default connect(mSTP, mDTP)(SessionModal)
