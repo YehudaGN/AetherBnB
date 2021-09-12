d2734980016c0fd6740807ec5f77daa2741a58b6
yudagn <yudagn@gmail.com>
2021-09-12 15:08:40 -0400

    fleshed out splash page a bit plus did some tweaks

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,101 +1,113 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
 
 class Header extends React.Component {
-    constructor(props) {
-        super(props);  
-        this.menuContainer = React.createRef();
-        this.state = {
-            open: false,
-            scrollClass: 'header-container-container'
-        };
-    }
-
-    componentDidMount() {
-        document.addEventListener("mousedown", this.handleClickOutside);
-        document.addEventListener("scroll", ()=> {
-            if (window.scrollY > 1) {
-                this.setState({scrollClass: "on-scroll"})
-            } else {
-                this.setState({scrollClass: "header-container-container"}) 
-            }
-
-        })
-    }
-
-    componentWillUnmount() {
-      document.removeEventListener("mousedown", this.handleClickOutside);
-    }
-
-    handleClickOutside = (event) => {
-        if (
-          this.menuContainer.current &&
-          !this.menuContainer.current.contains(event.target)
-        ) {
-          this.setState({
-            open: false,
-          });
-        }
-      };
-
-    sessionLinks = () => (
-        <ul className='login-signup-ul'>
-            <li className="menu-li" onClick={() => this.props.openModal('login')}>Log In</li>
-            <li className="menu-li" onClick={() => this.props.openModal('signup')}>Sign Up</li>
-        </ul>
-    );
-
-    handleClick = () => {
-        this.setState({open: !this.state.open});
+  constructor(props) {
+    super(props);
+    this.menuContainer = React.createRef();
+    this.state = {
+      open: false,
+      scrollClass: "header-container-container",
     };
+  }
 
-    render() {
-        return (
-            <div className='bgd'>  
-                <div className={this.state.scrollClass}>
-                    <div className='header-container'>       
-                        <div className='logo'>
-                            <Link className='logo-link' to='/'> 
-                                <img src={logo} alt="" />
-                            </Link> 
-                        </div >   
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+    document.addEventListener("scroll", () => {
+      if (window.scrollY > 1) {
+        this.setState({ scrollClass: "on-scroll" });
+      } else {
+        this.setState({ scrollClass: "header-container-container" });
+      }
+    });
+  }
 
-                        <div className='listings-search'>
-                            <Link className='link-search-text' to='/listings'>Listings</Link>  
-                            <SearchIcon className='search-icon'/>
-                        </div>
-                            
-                            <div className='menu-container' ref={this.menuContainer}>
-                                <button className='menu-button' onClick={this.handleClick}>
-                                    <MenuIcon style={{ fontSize: 30 }}/>
-                                    <AccountCircleIcon style={{ fontSize: 30 }}/>
-                                </button>
-                                {this.state.open && (
-                                <div className='dropdown'>
-                                    {this.props.currentUser ? 
-                                    <div className='dropdown-container'>
-                                        <div className='account-link-div'>
-                                            <Link className='account-link' to={`/users/show/${this.props.currentUser}`}>Account</Link>
-                                        </div>
-                                        <ul className='logout-ul'> 
-                                            <li className="menu-li" onClick={this.props.logout}>Log Out</li>
-                                        </ul>
-                                    </div>
-                                    :
-                                    this.sessionLinks()}
-                                </div>
-                                )}
-                            </div>
-                    </div>
-                </div>
-                <div className='header-cushion'></div>
-            </div>
-            
-        )
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  handleClickOutside = event => {
+    if (
+      this.menuContainer.current &&
+      !this.menuContainer.current.contains(event.target)
+    ) {
+      this.setState({
+        open: false,
+      });
     }
+  };
+
+  sessionLinks = () => (
+    <ul className="login-signup-ul">
+      <li className="menu-li" onClick={() => this.props.openModal("login")}>
+        Log In
+      </li>
+      <li className="menu-li" onClick={() => this.props.openModal("signup")}>
+        Sign Up
+      </li>
+    </ul>
+  );
+
+  handleClick = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    return (
+      <div className="bdd">
+        <div className={this.state.scrollClass}>
+          <div className="header-container">
+            <div className="logo">
+              <Link className="logo-link" to="/">
+                <img src={logo} alt="" />
+              </Link>
+            </div>
+
+            <div className="listings-search">
+              <Link className="link-search-text" to="/listings">
+                Listings
+              </Link>
+              <SearchIcon className="search-icon" />
+            </div>
+
+            <div className="menu-container" ref={this.menuContainer}>
+              <button className="menu-button" onClick={this.handleClick}>
+                <MenuIcon style={{ fontSize: 30 }} />
+                <AccountCircleIcon style={{ fontSize: 30 }} />
+              </button>
+              {this.state.open && (
+                <div className="dropdown">
+                  {this.props.currentUser ? (
+                    <div className="dropdown-container">
+                      <div className="account-link-div">
+                        <Link
+                          className="account-link"
+                          to={`/users/show/${this.props.currentUser}`}
+                        >
+                          Account
+                        </Link>
+                      </div>
+                      <ul className="logout-ul">
+                        <li className="menu-li" onClick={this.props.logout}>
+                          Log Out
+                        </li>
+                      </ul>
+                    </div>
+                  ) : (
+                    this.sessionLinks()
+                  )}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="header-cushion"></div>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,10 +1,61 @@
 import React from "react";
 const Splash = () => {
-    return(
-        <div>
-            <div className="splash-container"><img src={splash_bg} alt="" /></div>
+  return (
+    <div className="splash-container">
+      <div>
+        <div className="bg-img-container">
+          <img className="bg-img" src={splash_bg} alt="" height="800" />
         </div>
-    )     
-}
+      </div>
+      <div className="explore-container-container">
+        <div className="explore-h3-container">
+          <h3 className="explore-h3">Explore Places To Stay</h3>
+        </div>
+        <div className="explore-container">
+          <div className="explore-location">
+            <img className="location-img" src={window.night_sky} alt="" />
+            <p className="explore-location-text">New York</p>
+          </div>
+          <div className="explore-location">
+            <img className="location-img" src={window.night_sky} alt="" />
+            <p className="explore-location-text">Anchorage</p>
+          </div>
+          <div className="explore-location">
+            <img className="location-img" src={window.night_sky} alt="" />
+            <p className="explore-location-text">Philadelphia</p>
+          </div>
+          <div className="explore-location">
+            <img className="location-img" src={window.night_sky} alt="" />
+            <p className="explore-location-text">Boston</p>
+          </div>
+          <div className="explore-location">
+            <img className="location-img" src={window.night_sky} alt="" />
+            <p className="explore-location-text">Arlington</p>
+          </div>
+          <div className="explore-location">
+            <img className="location-img" src={window.night_sky} alt="" />
+            <p className="explore-location-text">Virginia Beach</p>
+          </div>
+          <div className="explore-location">
+            <img className="location-img" src={window.night_sky} alt="" />
+            <p className="explore-location-text">San Diego</p>
+          </div>
+          <div className="explore-location">
+            <img className="location-img" src={window.night_sky} alt="" />
+            <p className="explore-location-text">Austin</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-container-container">
+        <div className="footer-container">
+          <div className="about-h3-container">
+            <h3 className="about-h3">About</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Splash;
